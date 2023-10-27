b3bc5806741960826602d854678caab9877bcf66
Rust Saiargaliev <fly.amureki@gmail.com>
2023-10-27 10:44:22 +0200

    Separate dependency group for bakery

--- tests/test_baker_generators.py
+++ tests/test_baker_generators.py
@@ -1,7 +1,8 @@
-from model_bakery import baker
+import pytest
 
 from tests.testapp import models
 
 
 def test_generators():
+    baker = pytest.importorskip("model_bakery.baker")
     assert baker.prepare(models.TestModel)
